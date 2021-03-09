395e0042508b15097ac91b5dec6fb5a76d458f88
Henrique Guimaraes <henrique.guimaraes@dommus.com.br>
2021-03-09 00:01:58 -0300

    mensagem inicial

--- start/routes.js
+++ start/routes.js
@@ -2,5 +2,8 @@
 
 const Route = use('Route')
 
+Route.get("/", () => {
+    return { greeting: "Welcome to the Adonis API tutorial" };
+  });
 Route.post('/users', 'UserController.create')
 Route.post('/sessions', 'SessionController.create')
